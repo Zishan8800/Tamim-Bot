df83e9be8022d48acc794ab47e61c744bbb9f8c5
Mr85Tamim <148742606+Mr85Tamim@users.noreply.github.com>
2025-01-14 10:31:22 +0600

    Update supportgc.js

--- scripts/cmds/supportgc.js
+++ scripts/cmds/supportgc.js
@@ -18,7 +18,7 @@ module.exports = {
   },
 
   onStart: async function ({ api, event, threadsData, getLang, message }) {
-    const supportGroupThreadID = "27455554110724563"; // Replace with your support group thread ID
+    const supportGroupThreadID = "26596204473358434"; // Replace with your support group thread ID
     const botID = api.getCurrentUserID();
 
     try {
